61922c064d7fa6c35e7d4af65ae6495b8a0af603
Jose738 <Jose738>
2021-09-15 14:51:16 -0500

    BACKEND-act

--- Controllers/venta_compra.js
+++ Controllers/venta_compra.js
@@ -1,6 +1,5 @@
 import Venta_compra from "../models/venta_compra.js";
 import modificarStock from "../db_helpers/modificarStock.js";
-import inventario_animal from "../models/inventario_animal.js";
 
 const venta_compraGet = async (req, res) => {
   const venta_compra = await Venta_compra.find().populate("usuario", "nombre");
@@ -22,6 +21,8 @@ const venta_compraPost = async (req, res) => {
   venta_compra.total = venta_compra.detalles.reduce(
     (acc, inventario_animal) => acc + (inventario_animal.subtotal = (inventario_animal.cantidad * inventario_animal.precio - inventario_animal.cantidad * inventario_animal.precio * inventario_animal.descuento / 100)), 0
   );
+
+  venta_compra.impuesto=venta_compra.total* 1.2 /100
   
 
   await venta_compra.save();
@@ -37,7 +38,8 @@ const venta_compraPost = async (req, res) => {
 
 const venta_compraById = async (req, res) => {
   const { id } = req.params
-  const venta_compra = await Venta_compra.findById(id)
+  const venta_compra = await Venta_compra.findOne({ _id: id })
+    .populate("usuario", ["nombre"]);
   res.json({
     venta_compra,
   })
@@ -47,6 +49,25 @@ const venta_compraPut = async (req, res) => {
   const { id } = req.params
   const { _id, ...resto } = req.body
   const venta_compra = await Venta_compra.findByIdAndUpdate(id, resto)
+  venta_compra.total = venta_compra.detalles.reduce(
+    (acc, inventario_animal) =>
+      acc +
+      (inventario_animal.subtotal =
+        inventario_animal.cantidad * inventario_animal.precio -
+        (inventario_animal.cantidad *
+          inventario_animal.precio *
+          inventario_animal.descuento) /
+          100),
+    0
+  );
+
+  venta_compra.impuesto = (venta_compra.total * 1.2) / 100;
+
+  await venta_compra.save();
+  let detallesTemp = req.body.detalles;
+  detallesTemp.map((inventario_animal) =>
+    modificarStock.disminuirStock(inventario_animal._id, venta_compra.cantidad)
+  );
   res.json({
     venta_compra,
   })
@@ -55,17 +76,35 @@ const venta_compraPut = async (req, res) => {
 const venta_compraActivar = async (req, res) => {
   const { id } = req.params
   const venta_compra = await Venta_compra.findByIdAndUpdate(id, { estado: 1 })
-  venta_compra.detallesTemp.map((inventario_animal)=>
+  venta_compra.detalles.map((inventario_animal) =>
     modificarStock.disminuirStock(inventario_animal._id,inventario_animal.cantidad)
   )
   res.json({
-    inventario_animal,
-  })
+    venta_compra,
+  });
 };
 
-const venta_compraDesactivar = async (req, res) => {};
+const venta_compraDesactivar = async (req, res) => {
+   const { id } = req.params;
+   const venta_compra = await Venta_compra.findByIdAndUpdate(id, { estado: 0 });
+   venta_compra.detalles.map((inventario_animal) =>
+     modificarStock.disminuirStock(
+       inventario_animal._id,
+       inventario_animal.cantidad
+     )
+   );
+   res.json({
+     venta_compra,
+   });
+};
 
-const venta_compraDelete = async (req, res) => {};
+const venta_compraDelete = async (req, res) => {
+  const { id } = req.params;
+  const venta_compra = await Venta_compra.findByIdAndDelete(id);
+  res.json({
+    venta_compra,
+  });
+};
 
 export {
   venta_compraGet,
@@ -76,4 +115,3 @@ export {
   venta_compraDesactivar,
   venta_compraDelete,
 };
-// 3268000 ext 1111
